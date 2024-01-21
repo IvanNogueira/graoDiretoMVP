6b51a41ec4fa8f3f15e4b6a689fb4a3b4675d73d
Ivan Rezende <ivanrezendenogueira@hotmail.com>
2024-01-21 23:52:43 +0000

    alterações no front end

--- src/main/java/com/graodireto/mvp/security/AuthoritiesConstants.java
+++ src/main/java/com/graodireto/mvp/security/AuthoritiesConstants.java
@@ -9,6 +9,8 @@ public final class AuthoritiesConstants {
 
     public static final String USER = "ROLE_USER";
 
+    public static final String ESTABELECIMENTO = "ROLE_ESTABELECIMENTO";
+
     public static final String ANONYMOUS = "ROLE_ANONYMOUS";
 
     private AuthoritiesConstants() {}
--- src/main/webapp/app/home/home.component.ts
+++ src/main/webapp/app/home/home.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
 import { Subject } from 'rxjs';
-import { takeUntil } from 'rxjs/operators';
+import { map, takeUntil } from 'rxjs/operators';
 
 import SharedModule from 'app/shared/shared.module';
 import { AccountService } from 'app/core/auth/account.service';
 import { Account } from 'app/core/auth/account.model';
+import { IEstabelecimento } from 'app/entities/estabelecimento/estabelecimento.model';
+import { EstabelecimentoService } from 'app/entities/estabelecimento/service/estabelecimento.service';
 
 @Component({
   standalone: true,
@@ -18,17 +20,28 @@ export default class HomeComponent implements OnInit, OnDestroy {
   account: Account | null = null;
 
   private readonly destroy$ = new Subject<void>();
+  estabelecimentoCollection?: IEstabelecimento[];
 
   constructor(
     private accountService: AccountService,
     private router: Router,
+    private estabelecimentoService: EstabelecimentoService,
   ) {}
 
   ngOnInit(): void {
     this.accountService
       .getAuthenticationState()
       .pipe(takeUntil(this.destroy$))
-      .subscribe(account => (this.account = account));
+      .subscribe(account => ((this.account = account), console.log(this.account)));
+
+    this.estabelecimentoService.query().subscribe(
+      res => {
+        this.estabelecimentoCollection = res.body!;
+      },
+      error => {
+        console.error('Erro:', error);
+      },
+    );
   }
 
   login(): void {
